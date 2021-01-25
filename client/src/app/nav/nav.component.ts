15fce29b924d237f415354cdc94e9c374972aeb8
bursztynsky <bursztynski.tomasz@gmail.com>
2021-01-25 20:43:07 +0100

    End of section 7

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -13,26 +13,21 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
   model: any = {}
 
-  constructor(public accountService: AccountService, 
+  constructor(public accountService: AccountService,
     private router: Router,
     private toastr: ToastrService
-    ) { }
+  ) { }
 
   ngOnInit(): void {
   }
 
-  login(){
-    this.accountService.login(this.model).subscribe(response => 
-      {
-        this.router.navigateByUrl('/members');
-      }, error => 
-      {
-        console.log(error);
-        this.toastr.error(error.error);
-      })
+  login() {
+    this.accountService.login(this.model).subscribe(response => {
+      this.router.navigateByUrl('/members');
+    })
   }
 
-  logout(){
+  logout() {
     this.accountService.logout();
     this.router.navigateByUrl('/');
   }
